f80040e1fe5df4f9a85f2a5d9cd27fc7a4cc4b48
Alex Rusin <Rusin.AI@phystech.edu>
2020-10-30 00:53:10 +0300

    minor

--- System_window.hpp
+++ System_window.hpp
@@ -28,14 +28,23 @@ public:
         draw_start.y += height_ - angle_circle_y;
         Engine::draw_rectangle(draw_start, width_ - 2 * angle_circle_x, angle_circle_y, COLORS::sys_window_top);
 
-
         draw_start = start_;
         draw_start.y += angle_circle_y;
         Engine::draw_rectangle(draw_start, width_, height_ - 2 * angle_circle_y, back_color_);
 
+        draw_start = start_;
+        draw_start.x += angle_circle_x;
+        draw_start.y += angle_circle_y;
+        Engine::draw_circle(draw_start, angle_circle_x, back_color_);
 
+        draw_start = start_;
+        draw_start.x += width_  - angle_circle_x;
+        draw_start.y += angle_circle_y;
+        Engine::draw_circle(draw_start, angle_circle_x, back_color_);
         
-        
+        draw_start = start_;
+        draw_start.x += angle_circle_x;
+        Engine::draw_rectangle(draw_start, width_ - 2 * angle_circle_x, angle_circle_y, back_color_);
     }
 
     bool handle() {
